cbd020d12f1215e8b022938a5d7f5ccc68c081b0
pengtang <cootertang@gmail.com>
2015-04-05 11:38:46 -0400

    upgrade the teleop function, add dependencies for the previous commit

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -39,9 +39,9 @@ void quit(int sig)
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "teleop_RosAria");
-  TeleopRosAria teleop_turtle;
+  TeleopRosAria teleop_RosAria;
   signal(SIGINT,quit);
-  teleop_turtle.keyLoop();
+  teleop_RosAria.keyLoop();
   return(0);
 }
 void TeleopRosAria::keyLoop()
@@ -59,6 +59,7 @@ void TeleopRosAria::keyLoop()
   puts("Reading from keyboard");
   puts("---------------------------");
   puts("Use arrow keys to move the robot.");
+  puts("Press Q to stop the robot.");
   for(;;)
     {
       // get the next event from the keyboard
@@ -73,22 +74,32 @@ void TeleopRosAria::keyLoop()
 	{
 	case KEYCODE_L:
 	  ROS_DEBUG("LEFT");
-	  angular_ = 1.0;
+	  angular_ = 0.1;
+	  linear_ = 0;
 	  dirty = true;
 	  break;
 	case KEYCODE_R:
 	  ROS_DEBUG("RIGHT");
-	  angular_ = -1.0;
+	  angular_ = -0.1;
+	  linear_ = 0;
 	  dirty = true;
 	  break;
 	case KEYCODE_U:
 	  ROS_DEBUG("UP");
-	  linear_ = 1.0;
+	  linear_ = 0.1;
+	  angular_ = 0;
 	  dirty = true;
 	  break;
 	case KEYCODE_D:
 	  ROS_DEBUG("DOWN");
-	  linear_ = -1.0;
+	  linear_ = -0.1;
+	  angular_ = 0;
+	  dirty = true;
+	  break;
+	case KEYCODE_Q:
+	  ROS_DEBUG("STOP");
+	  linear_ = 0;
+	  angular_ = 0;
 	  dirty = true;
 	  break;
 	}
