4f85c8b624318c103f2373c40666ebf033bf2098
Alex Keyes <alex.o.keyes@gmail.com>
2015-04-16 19:18:22 -0400

    added a working Interface

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# rosaria_client
 This is the rosaria client
 
-Beta V1.0
-Most of the time it works fine, sometimes it does not stop(continue going ahead or continue spinning).
+Beta V1.0 Most of the time it works fine, sometimes it does not stop(continue going ahead or continue spinning).
 
-Beta V1.1 (Mar. 28)
-Fix the bug sometimes the robot does not stop
-Add feature of Stopping the robot first, and then do its functionality 
+Beta V1.1 (Mar. 28) Fix the bug sometimes the robot does not stop Add feature of Stopping the robot first, and then do its functionality
 
-Beta V1.2 (Apr. 5)
-Add print_state function(could print part of the info right now, need to be enhanced).
+Beta V1.2 (Apr. 5) Add print_state function(could print part of the info right now, need to be enhanced).
 
-Beta V1.3 (Apr. 10)
-Pushed some small corrections to spin_clockwise, renamed spin_anticlockwise to spin_counterclockwise and added small corrections to that as well.  Also added (cery) rough draft of interface program. 
+Beta V1.3 (Apr. 10) Pushed some small corrections to spin_clockwise, renamed spin_anticlockwise to spin_counterclockwise and added small corrections to that as well. Also added (very) rough draft of interface program.
+
+Beta V1.4 (Apr. 10) Change the spin from 180 to 90 degree, finished the print_state function.
+
+Beta V1.5 (Apr. 14) Add function enable_motors, fix the printing information about the front_motors and rear_motors state.
+
+Beta V1.6(Apr 16) Added a functional version of the interface 
--- src/interface.cpp
+++ src/interface.cpp
@@ -2,23 +2,60 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#define KEYCODE_1 0x31 
+#define KEYCODE_2 0X32
+#define KEYCODE_3 0x33
+#define KEYCODE_4 0x34
+#define KEYCODE_5 0x35
+#define KEYCODE_Q 0X71
+
 
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "Interface");
-	ros::NodeHandle nh;
+  ros::init(argc, argv, "RosAria_interface");
+  ros::NodeHandle nh;
 
-	ROS_INFO_STREAM("Please enter 1 or 2");
+  std::cout  << "******************************************************************" << std::endl
+	     << "*                   ROSARIA CLIENT INTERFACE                     *" << std::endl
+	     << "*                                                                *" << std::endl
+	     << "*            Welcome to the RosAria client interface!            *" << std::endl
+	     << "*                                                                *" << std::endl
+	     << "*       [1] go_three_second                                      *" << std::endl
+	     << "*       [2] spin_clockwise                                       *" << std::endl
+	     << "*       [3] spin_counterclockwise                                *" << std::endl
+	     << "*       [4] teleop                                               *" << std::endl
+	     << "*       [5] print_state                                          *" << std::endl
+	     << "*       Press [Q] to close the interface                         *" << std::endl 
+	     << "******************************************************************" << std::endl;
 
-	int i;
-	//int cond;
-	//cin >> cond;
+  char select,a;
 
-	//if(cond == 1)
-		i = system("rosrun rosaria_client go_three_second");
-	//else if (cond == 2)
-	//	i = system("rosrun rosaria_client spin_clockwise");
-
-	return 1;
+  while(ros::ok()){
+    std::cin >> select;	
+    switch(select)
+      {
+      case KEYCODE_1:
+	a = system("rosrun rosaria_client go_three_second");
+	break;
+      case KEYCODE_2:
+	a = system("rosrun rosaria_client spin_clockwise");
+	break;
+      case KEYCODE_3:
+	a = system("rosrun rosaria_client spin_counterclockwise");
+	break;
+      case KEYCODE_4:
+	a = system("rosrun rosaria_client teleop");
+	break;
+      case KEYCODE_5:
+	a = system("rosrun rosaria_client print_state");
+	break;
+      case KEYCODE_Q:
+	return false;
+	break;
+      default:
+	std::cout << "Please enter a number from the list or Q to quit." << std::endl;
+      }
+  }
+  return 1;
 }
