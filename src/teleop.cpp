b1157a7f63e3c891a1a3c704cbd7f6991da38d6d
pengtang <cootertang@gmail.com>
2015-04-18 14:51:05 -0400

    modify teleop

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -3,21 +3,23 @@
 #include <signal.h>
 #include <termios.h>
 #include <stdio.h>
-#define KEYCODE_SPC 0x20
+
 #define KEYCODE_R 0x43
 #define KEYCODE_L 0x44
 #define KEYCODE_U 0x41
 #define KEYCODE_D 0x42
 #define KEYCODE_Q 0x71
+#define KEYCODE_SPACE 0x20
+
 class TeleopRosAria
 {
-public:
-  TeleopRosAria();
-  void keyLoop();
-private:
-  ros::NodeHandle nh_;
-  double linear_, angular_, l_scale_, a_scale_;
-  ros::Publisher twist_pub_;
+  public:
+    TeleopRosAria();
+    void keyLoop();
+  private:
+    ros::NodeHandle nh_;
+    double linear_, angular_, l_scale_, a_scale_;
+    ros::Publisher twist_pub_;
 };
 TeleopRosAria::TeleopRosAria():
   linear_(0),
@@ -61,61 +63,63 @@ void TeleopRosAria::keyLoop()
   puts("---------------------------");
   puts("Use arrow keys to move the robot.");
   puts("Press the space bar to stop the robot.");
+  puts("Press q to stop the program");
   for(;;)
-    {
-      // get the next event from the keyboard
-      if(read(kfd, &c, 1) < 0)
-	{
-	  perror("read():");
-	  exit(-1);
-	}
-      linear_=angular_=0;
-      ROS_DEBUG("value: 0x%02X\n", c);
-      switch(c)
-	{
-	case KEYCODE_L:
-	  ROS_DEBUG("LEFT");
-	  angular_ = 0.1;
-	  linear_ = 0;
-	  dirty = true;
-	  break;
-	case KEYCODE_R:
-	  ROS_DEBUG("RIGHT");
-	  angular_ = -0.1;
-	  linear_ = 0;
-	  dirty = true;
-	  break;
-	case KEYCODE_U:
-	  ROS_DEBUG("UP");
-	  linear_ = 0.1;
-	  angular_ = 0;
-	  dirty = true;
-	  break;
-	case KEYCODE_D:
-	  ROS_DEBUG("DOWN");
-	  linear_ = -0.1;
-	  angular_ = 0;
-	  dirty = true;
-	  break;
-	case KEYCODE_SPC:
-	  ROS_DEBUG("STOP");
-	  linear_ = 0;
-	  angular_ = 0;
-	  dirty = true;
-	  break;
-  case KEYCODE_Q:
-    ROS_DEBUG("QUIT");
-    quit(1);
-    break;
-	}
-      geometry_msgs::Twist twist;
-      twist.angular.z = a_scale_*angular_;
-      twist.linear.x = l_scale_*linear_;
-      if(dirty ==true)
-	{
-	  twist_pub_.publish(twist);
-	  dirty=false;
-	}
-    }
+  {
+    // get the next event from the keyboard
+    if(read(kfd, &c, 1) < 0)
+	  {
+  	  perror("read():");
+  	  exit(-1);
+	  }
+    linear_=angular_=0;
+    ROS_DEBUG("value: 0x%02X\n", c);
+    switch(c)
+	  {
+    	case KEYCODE_L:
+    	  ROS_DEBUG("LEFT");
+    	  angular_ = 0.1;
+    	  linear_ = 0;
+    	  dirty = true;
+    	  break;
+    	case KEYCODE_R:
+    	  ROS_DEBUG("RIGHT");
+    	  angular_ = -0.1;
+    	  linear_ = 0;
+    	  dirty = true;
+    	  break;
+    	case KEYCODE_U:
+    	  ROS_DEBUG("UP");
+    	  linear_ = 0.1;
+    	  angular_ = 0;
+    	  dirty = true;
+    	  break;
+    	case KEYCODE_D:
+    	  ROS_DEBUG("DOWN");
+    	  linear_ = -0.1;
+    	  angular_ = 0;
+    	  dirty = true;
+    	  break;
+    	case KEYCODE_SPACE:
+    	  ROS_DEBUG("STOP");
+    	  linear_ = 0;
+    	  angular_ = 0;
+    	  dirty = true;
+    	  break;
+      case KEYCODE_Q:
+        ROS_DEBUG("QUIT");
+        ROS_INFO_STREAM("You quit the teleop successfully");
+        return;
+        break;
+  	}
+    geometry_msgs::Twist twist;
+    twist.angular.z = a_scale_*angular_;
+    twist.linear.x = l_scale_*linear_;
+    if(dirty ==true)
+  	{
+  	  twist_pub_.publish(twist);
+  	  dirty=false;
+  	}
+  }
   return;
 }
