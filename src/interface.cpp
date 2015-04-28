50c9931166b3b7af70b9d1075e406ca58d7a556b
Alex Keyes <alex.o.keyes@gmail.com>
2015-04-28 01:53:13 -0400

    updated interface

--- src/interface.cpp
+++ src/interface.cpp
@@ -11,7 +11,8 @@
 #define KEYCODE_6 0x36
 #define KEYCODE_Q 0X71
 
-static struct termios old, new1;
+static struct termios new1, old;
+
 void initTermios(int echo) {
     tcgetattr(0, &old); /* grab old terminal i/o settings */
     new1 = old; /* make new settings same as old settings */
@@ -23,15 +24,19 @@ void initTermios(int echo) {
 /* Restore old terminal i/o settings */
 void resetTermios() {
     tcsetattr(0, TCSANOW, &old);
-}
+    }
 
+/* Handles top level control as usual */
 int main(int argc, char **argv)
 {
+  /* initialize the ros node */
 	ros::init(argc, argv, "RosAria_interface");
 	ros::NodeHandle nh;
 
+  /* change the terminal input settings */ 
 	initTermios(0);
 
+  /* greet user and display selection options */
 	std::cout  	<< "******************************************************************" << std::endl
 							<< "*                   ROSARIA CLIENT INTERFACE                     *" << std::endl
 							<< "*                                                                *" << std::endl
@@ -46,38 +51,39 @@ int main(int argc, char **argv)
 							<< "*       Press [Q] to close the interface                         *" << std::endl 
 							<< "******************************************************************" << std::endl;
 
-	char select,a;
+	char select,a; /* vars to be used in switch statement */
 
+  /* loop to handle user input */
 	while(ros::ok()){
-		std::cout << "Please select a program to run, or hit q to quit: "<< std::endl;
-		std::cin >> select;	
+		std::cout << "Please select a program to run, or hit q to quit: "<< std::endl; /* prompt user at start of every loop */
+		std::cin >> select;	/* use standard input to select program to run */
 		switch(select)
 		{
 			case KEYCODE_1:
-				a = system("rosrun rosaria_client go_three_second");
+				a = system("rosrun rosaria_client go_three_second"); /* run option 1 */
 				break;
 			case KEYCODE_2:
-				a = system("rosrun rosaria_client spin_clockwise");
+				a = system("rosrun rosaria_client spin_clockwise"); /* run option 2 */
 				break;
 			case KEYCODE_3:
-				a = system("rosrun rosaria_client spin_counterclockwise");
+				a = system("rosrun rosaria_client spin_counterclockwise"); /* run option 3 */
 				break;
 			case KEYCODE_4:
-				a = system("rosrun rosaria_client teleop");
+				a = system("rosrun rosaria_client teleop"); /* run option 4 */
 				break;
 			case KEYCODE_5:
-				a = system("rosrun rosaria_client print_state");
+				a = system("rosrun rosaria_client print_state"); /* run option 5 */
 				break;
 			case KEYCODE_6:
-				a = system("rosrun rosaria_client enable_motors");
+				a = system("rosrun rosaria_client enable_motors"); /* run option 6 */
 				break;
-			case KEYCODE_Q:
-				return false;
+			case KEYCODE_Q: /* quit the interface program */
+        resetTermios(); /* reset the terminal back to old settings */
+				return false; /* close loop and end node */
 				break;
-			default:
+			default: /* in case of user not entering selection from list */
 			std::cout << "Please enter a number from the list or Q to quit." << std::endl;
 		}
 	}
-	resetTermios();
 	return 1;
 }
